657620682a9c9a50d9820ee48e966e86b66bad36
Jen412 <72884006+Jen412@users.noreply.github.com>
2021-05-16 22:33:19 -0500

    CAmbios 16/05/21

--- nosotros.php
+++ nosotros.php
@@ -2,11 +2,21 @@
 require 'includes/funciones.php';
 inlcuirTemplate("header"); 
 ?>
-    <main class="contenedor seccion">
-        <h1>Nosotros</h1>
-
-        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eaque cum magni adipisci dolorem quo commodi at vitae rem maxime ex ipsam voluptate aliquid explicabo porro velit, aspernatur libero delectus?</P>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cupiditate dolorum laborum hic quia possimus, velit, perspiciatis earum ipsam, laudantium nihil. Fuga necessitatibus esse voluptatem nihil! Nesciunt debitis autem quisquam.</p>
+    <main class="contenedor seccion nosotros">
+        <h1>Conoce Más Sobre Nosotros</h1>
+        <blockquote class="derecha">20 años de Experiencia</blockquote>
+        <div class="nosotrosCon">
+            <picture>
+                <source srcset="build/img/nosotros.webp" type="image/webp">
+                <source srcset="build/img/nosotros.jpg" type="image/jpeg">
+                <img src="build/img/nosotros.jpg" alt="Imagen Nosotros" loading="lazy">
+            </picture>    
+            <div class="contenido-nosotros">
+                <p class="justify">Nuestro bufete jurídico es reconocido por muchos tribunales por brindar un trabajo de excelente calidad y cumplir con brindar un servicio de la más alta expectativa.</p>
+                <p class="justify">Nosotros atendemos cualquier tipo de asunto civil y judicial que se nos presente al momento.</p>
+                <p class="justify">Y recuerda, si tienes un problema, siempre hay una solución legal al alcance de tu mano!</p>
+            </div>
+        </div>
     </main>
 
 <?php inlcuirTemplate("footer");   ?>
