5e50be47bd85b6b58acbe4cee2f08c88e82dea6c
Jen412 <72884006+Jen412@users.noreply.github.com>
2021-05-30 22:22:39 -0500

    Cambios 30/05/21
    Se implemento la descarga de archivos en documentosCaso.php

--- admin/Casos/agregarDocumento.php
+++ admin/Casos/agregarDocumento.php
@@ -1,16 +1,78 @@
 <?php 
-require '../../includes/funciones.php';
-inlcuirTemplate("header"); 
+    require '../../includes/funciones.php';
+    require '../../includes/config/database.php';
+
+    $auth = estaAutenticado();
+    if (!$auth) {
+        header('location: /');
+    }
+    inlcuirTemplate("header"); 
+    $db = conectarDB();
+
+    $id = $_GET['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+    //si id lo cambian regresa 
+    if (!$id) {
+        header('Location: /admin/Casos/documentosCaso.php');
+    }
+    //Posibles errores
+    $errores =[];
+    $nombreDocCaso='';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        //echo "<pre>";
+        //var_dump($_FILES);
+        //echo "</pre>";
+        $nombreDocCaso = mysqli_real_escape_string($db, $_POST['nomDoc']);
+        $doc = $_FILES['doc'];
+        
+
+        if (!$doc) {
+            $errores[] = 'Documento Obligatorio';
+        }
+        if (!$nombreDocCaso) {
+            $errores[] = 'Nombre Obligatorio';
+        }
+
+        
+        if (empty($errores)) {
+            $carpetaDocCaso = '../../DocumentosCaso/';
+            if (!is_dir($carpetaDocCaso)) {
+                mkdir($carpetaDocCaso);
+            }
+            $extension = pathinfo($doc['name'], PATHINFO_EXTENSION);
+            $nombreDocCaso = trim($nombreDocCaso);
+            $nombreDocCaso = str_replace(" ", "", $nombreDocCaso);
+            $docCaso = $nombreDocCaso.'.'.$extension;
+            $query = "CALL agregarDocCaso('$id', '$docCaso','$nombreDocCaso');";
+            $resultado = mysqli_query($db, $query);
+            move_uploaded_file($doc['tmp_name'], $carpetaDocCaso.$docCaso);
+            if ($resultado) {
+                header("Location: /admin/Casos/documentosCaso.php?id=$id");
+            }
+
+        }
+    }
+
 ?>
     <main class="contenedor seccion">
         <h1>Agregar Documento</h1>
-        <a href="/admin/Casos/documentosCaso.php" class="boton-azul">Volver</a>
-        <form action="" class="formulario">
+        <a href="/admin/Casos/documentosCaso.php?id=<?php echo $id?>" class="boton-azul">Volver</a>
+
+        <?php foreach($errores as $error): ?>
+            <div class="alerta error">
+                <?php        echo $error; ?>
+            </div>
+        <?php    endforeach;?>
+        <form action="" class="formulario" method="POST" enctype="multipart/form-data">
             <fieldset>
                 <legend>Documento</legend>
+                <label for="nomDoc">Nombre Documento</label>
+                <input type="text" name="nomDoc" id="nomDoc" value="<?php echo $nombreDocCaso;?>">    
                 <label for="doc">Documento</label>
                 <input type="file" name="doc" id="doc">
             </fieldset>
+            <input type="submit" class="boton-azul">
         </form>
     </main>
 
--- admin/Casos/documentosCaso.php
+++ admin/Casos/documentosCaso.php
@@ -3,15 +3,51 @@
     require '../../includes/config/database.php';
     
     $db = conectarDB();
-    $query= "SELECT * FROM doccaso";
+    $resultadoAccion = $_GET['resultado'] ?? null;
+    $id = $_GET['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+    //si id lo cambian regresa 
+    if (!$id) {
+        header('Location: /admin/Casos/casos.php');
+    }
+    $query= "CALL consultaTodDocCaso('$id');";
     $resultado = mysqli_query($db, $query);
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $idDoc = $_POST['id'];
+        $nomDoc =$_POST['nombreDoc'];
+        $idDoc = filter_var($idDoc, FILTER_VALIDATE_INT);
+        if($idDoc){
+            //Elimina DocCaso de la raiz
+            $db = conectarDB();
+            $query ="CALL consultaDocCaso('$id', '$nomDoc')";
+            $resultadoE = mysqli_query($db,$query);
+            $documento = mysqli_fetch_assoc($resultadoE);
+            $carpetaDocCaso = '../../DocumentosCaso/';
+            unlink($carpetaDocCaso.$documento['URL_Doc']);
+            //Elimina DocCaso
+            $db = conectarDB();
+            $query = "CALL EliminarDocCaso('$idDoc','$nomDoc')";
+            $resultadoE = mysqli_query($db, $query);
+            if ($resultadoE) {
+                header("Location: /admin/Casos/documentosCaso.php?id=$id&resultado=3");
+            }
+        }
+    }
     inlcuirTemplate("header");
 
 ?>
     <main class="contenedor seccion">
         <h1>Documentos</h1>
+
+        <?php if (intval($resultadoAccion) === 1): ?>
+            <p class="alerta exito">Documento Agregado Correctamente</p>
+        <?php elseif (intval($resultadoAccion) === 2):?>
+            <p class="alerta exito">Documento Eliminado Correctamente</p>     
+        <?php endif;?>
+
         <div class="acciones">
-            <a href="/admin/Casos/agregarDocumento.php?id=1" class="boton-azul">Agregar Documento</a>
+            <a href="/admin/Casos/agregarDocumento.php?id=<?php echo $id?>" class="boton-azul">Agregar Documento</a>
             <a href="/admin/Casos/casos.php"  class="boton-azul">Volver</a>
         </div>
         <table class="documentos">
@@ -26,12 +62,15 @@
                 <tr>
                     <td><?php echo $doc['Nombre_Doc']?></td>
                     <td>
-                        <form action="">
-                            
+                        <form action="" method="POST" class="w-100">
+                            <input type="hidden" name="id" value = "<?php echo $doc['Id_DocCaso'];?>">
+                            <input type="hidden" name="nombreDoc" value="<?php echo$doc['Nombre_Doc'];?>">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
                         </form>
+                        <a href="/DocumentosCaso/<?php echo $doc['URL_Doc'];?>" download="<?php echo $doc['URL_Doc'];?>" class="boton-verde-block">Descargar</a>
                     </td>
                 </tr>
-                <?php endwhile;?>
+                <?php endwhile; ?>
             </tbody>
         </table>
     </main>
